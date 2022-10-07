06f24a2cb51f9246a3fec46543d5399d17a39cb3
A-GX <antoine.gansel@tutanota.com>
2022-10-07 19:18:49 +0200

    almost done

--- Scanner/check_ct.py
+++ Scanner/check_ct.py
@@ -3,33 +3,58 @@ from base64 import b64encode
 from pprint import pprint
 
 f = open("final-out/ct-by-chain", 'w')
+d = open("final-out/ctName-by-chain.sorted", 'r')
+dict = {}
+dic = d.readlines()
+for l in dic :
+    l = l.split(':')
+    dict[l[0]] = l[-2]
+    print(l)
+
 
 with open('final-out/list-ct-chain', 'r') as fobj:
     data = fobj.readlines()
 
 logs = {}
 seen = {}
+seen_org = {
+    'Google' : False,
+    'DigiCert' : False,
+    'Cloudflare' : False,
+    "Let's Encrypt" : False,
+    'Sectigo' : False,
+}
+
+org = {
+    'Google' : 0,
+    'DigiCert' : 0,
+    'Cloudflare' : 0,
+    "Let's Encrypt" : 0,
+    'Sectigo' : 0,
+}
 tot = 0
 
 for l in data:
     test = bytearray.fromhex(l[3:-2].replace(':', ''))
     test = b64encode(test).decode()
-    print(test)
     if l == '[\n' or l == ']\n':
+        tot += 0.5
         for k in seen.keys():
             seen[k] = False
+        for k in seen_org.keys():
+            seen_org[k] = False
     else :
         if not l[3:-2] in seen.keys() or seen[l[3:-2]]==False:
-            print("ok")
             seen[l[3:-2]] = True
-            tot += 1
             if not (test in logs.keys()):
-                print("sad")
                 logs[test] = 1
             else :
                 logs[test] += 1
+            if not seen_org[dict[test]] :
+                org[dict[test]] += 1
+                seen_org[dict[test]] = True
 
 for i in logs.keys():
-    f.write(i + ' ' + str(logs[i]) + ' ' + str(logs[i]/tot) + '\n')
+    f.write(i + ' ' + str(logs[i]) + ' ' + str(logs[i]/tot) + ' ' + dict[i] + ' ' + str(org[dict[i]]/tot) + '\n')
 
 f.close()
